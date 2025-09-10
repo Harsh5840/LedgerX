53d645dc883d629e2415326a594b65df6b3ef431
Harsh5840 <shuklaharsh5840@gmail.com>
2025-09-10 23:38:14 +0530

    hoija

--- apps/ledgerX-backend/src/middleware/loggerMiddleware.ts
+++ apps/ledgerX-backend/src/middleware/loggerMiddleware.ts
@@ -8,7 +8,7 @@ export function requestLogger(req: Request, res: Response, next: NextFunction) {
     const duration = Date.now() - start;
     const userId = req.user?.id || "guest";
 
-    .toISOString()}] ${req.method} ${req.originalUrl} - ${res.statusCode} (${duration}ms) [user: ${userId}]`);
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl} - ${res.statusCode} (${duration}ms) [user: ${userId}]`);
   });
 
   next();
